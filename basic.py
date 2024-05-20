f8ab8a8f68b102925aeb431f1ab34b78b4179771
OM PRAKASH SAH TELI <omprakashsah4044@gmail.com>
2024-05-20 10:36:11 +0530

    udated basic python code

---
+++ basic.py
@@ -0,0 +1,10 @@
+num = int(input("Enter the first number:- "))
+num1 = int(input("Enter the first number:- "))
+sum = num+num1
+diff = num -num1
+print(sum)
+print(diff)
+# Enter the first number:- 7
+# Enter the first number:- 5
+# 12
+# 2
